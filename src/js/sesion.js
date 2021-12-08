71e7117785407a27d7570cffb2bb024f425929dd
oztuk9 <oztuk214@gmail.com>
2021-12-08 07:56:55 -0800

    Correciones visuales desde la laptop

--- src/js/sesion.js
+++ src/js/sesion.js
@@ -1,8 +1,16 @@
 const storage = require("../js/local");
 
-console.log(storage.getStorage("idUsuario").sesionIniciada);
-if ((storage.getStorage("idUsuario").sesionIniciada)==true) {
-    location.href='./gestion.html';
-}else{
-    location.href='./login.html';
+if (localStorage.getItem("idUsuario") === null) {
+    let idUsuario = {
+        id: storage.getStorage("idProducto").id,
+        editar: false
+      }
+      storage.setStorage("idUsuario", idUsuario);
+} else {
+    console.log(storage.getStorage("idUsuario").sesionIniciada);
+    if ((storage.getStorage("idUsuario").sesionIniciada) == true) {
+        location.href = './gestion.html';
+    } else {
+        location.href = './login.html';
+    }
 }
